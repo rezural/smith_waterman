b0cddd19f41d2c58babcc067c96a06dc482dc46e
Stephen Becker IV <github@deathbyescalator.com>
2015-06-21 09:34:17 -0700

    Rename variables and flip cols with rows
    Better match to example found here
    http://puriney.github.io/2013/08/22/smith-waterman-algorithm/

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@ use nalgebra::DMat;
 use std::fmt::{Debug, Formatter, Result};
 
 pub struct SmithWaterman{
-    sequence1: String,
-    sequence2: String,
+    genome_sequence: String,
+    read_sequence: String,
     matrix:  DMat<isize>,
     matched: isize,
     missed: isize,
@@ -15,9 +15,9 @@ pub enum GraphMovements {Blank, Left, Top, Diagonal}
 
 impl SmithWaterman{
 
-    pub fn new(sequence1: String, sequence2: String) -> SmithWaterman {
-        SmithWaterman{matrix: nalgebra::DMat::new_zeros(sequence1.len()+1, sequence2.len()+1),
-        sequence1: sequence1, sequence2: sequence2, matched: 2, missed: -1}
+    pub fn new(genome_sequence: String, read_sequence: String) -> SmithWaterman {
+        SmithWaterman{matrix: nalgebra::DMat::new_zeros(read_sequence.len()+1, genome_sequence.len()+1),
+        genome_sequence: genome_sequence, read_sequence: read_sequence, matched: 2, missed: -1}
     }
 
     fn penalty(&self, value: isize, penalty_value: isize) -> isize{
@@ -32,12 +32,12 @@ impl SmithWaterman{
     pub fn score(&mut self){
         let mut max_point = (0,0);
         let mut max = 0;
-        for row in (1..self.sequence1.len()+1){
-            for col in (1..self.sequence2.len()+1){
+        for row in (1..self.read_sequence.len()+1){
+            for col in (1..self.genome_sequence.len()+1){
                 let left = self.penalty(self.matrix[(row, col-1)], self.missed);
                 let top = self.penalty(self.matrix[(row-1, col)], self.missed);
                 let diagonal = self.matrix[(row-1, col-1)];
-                let diagonal_match = if self.sequence2.char_at(col-1) == self.sequence1.char_at(row-1){
+                let diagonal_match = if self.read_sequence.char_at(row-1) == self.genome_sequence.char_at(col-1){
                     self.penalty(diagonal, self.matched)
                 }else{
                     self.penalty(diagonal, self.missed)
@@ -51,28 +51,28 @@ impl SmithWaterman{
             }
         }
         let mut last_movement = GraphMovements::Blank;
-        let mut sequence1_alignment = String::new();
-        let mut sequence2_alignment = String::new();
+        let mut genome_sequence_alignment = String::new();
+        let mut read_sequence_alignment = String::new();
         while max > 0 {
             let (row, col) = max_point;
-            let one = self.sequence1.char_at(row-1);
-            let two = self.sequence2.char_at(col-1);
+            let one = self.genome_sequence.char_at(col-1);
+            let two = self.read_sequence.char_at(row-1);
             match last_movement{
                 GraphMovements::Blank  => {
-                    sequence1_alignment.push(one);
-                    sequence2_alignment.push(two);
+                    genome_sequence_alignment.push(one);
+                    read_sequence_alignment.push(two);
                 },
                 GraphMovements::Diagonal => {
-                    sequence1_alignment.push(one);
-                    sequence2_alignment.push(two);
+                    genome_sequence_alignment.push(one);
+                    read_sequence_alignment.push(two);
                 },
                 GraphMovements::Top => {
-                    sequence1_alignment.push(one);
-                    sequence2_alignment.push('-');
+                    genome_sequence_alignment.push(one);
+                    read_sequence_alignment.push('-');
                 },
                 GraphMovements::Left => {
-                    sequence1_alignment.push(one);
-                    sequence2_alignment.push('-');
+                    genome_sequence_alignment.push(one);
+                    read_sequence_alignment.push('-');
                 },
             }
             let top = self.matrix[(row-1, col)];
@@ -90,8 +90,8 @@ impl SmithWaterman{
                 (row-1, col)
             };
         };
-        let x1: String = sequence1_alignment.chars().rev().collect();
-        let x2: String = sequence2_alignment.chars().rev().collect();
+        let x1: String = genome_sequence_alignment.chars().rev().collect();
+        let x2: String = read_sequence_alignment.chars().rev().collect();
         println!("{:?}",x1);
         println!("{:?}",x2);
     }
@@ -103,9 +103,9 @@ impl Debug for SmithWaterman {
         for row in 0..self.matrix.nrows()+1 {
             for col in 0..self.matrix.ncols()+1 {
                 let _ = if col==0 && row>1{
-                    write!(form, "{:>5}", self.sequence1.char_at(row-2).to_string())
+                    write!(form, "{:>5}", self.read_sequence.char_at(row-2).to_string())
                 } else if row==0 && col>1{
-                    write!(form, "{:>5}", self.sequence2.char_at(col-2).to_string())
+                    write!(form, "{:>5}", self.genome_sequence.char_at(col-2).to_string())
                 } else if row>=1 && col>=1{
                     write!(form, "{:>5}", self.matrix[(row-1,col-1)])
                 }else{
